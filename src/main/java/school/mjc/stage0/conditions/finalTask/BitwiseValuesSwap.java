95dd3ccecd069a5eb81ae563a68e519197efc51e
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-14 17:10:13 +0300

    feat: complete finalTask module4.1 v.8

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        //first = first^second^(second = first);
 
         first = first ^ second;
         second = first ^ second;
