3ae98f281fffdc2c8ea062d533ff8f535ddcee8e
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-14 16:18:01 +0300

    feat: complete finalTask module4.1 v.0

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int res;
+        if (divider != 0) {
+            res = dividend / divider;
+            System.out.println(res * divider == dividend
+                                ? "can be divided completely"
+                                : "cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
     }
 }
